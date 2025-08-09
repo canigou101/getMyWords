1a6278f97993bac55316bd3c8f961d0802529e5c
canigou101 <127309239+canigou101@users.noreply.github.com>
2025-08-09 17:38:03 +0200

    update

--- script/layout.gd
+++ script/layout.gd
@@ -5,7 +5,8 @@ var MultiplicateurDePoints=1
 
 func _on_button_pressed():
 	NbPoints+=(1*MultiplicateurDePoints)
+	afficherPoints(NbPoints)
 	
 func afficherPoints(nombre):
-	var affichage ="points :"+nombre
-	
+	var affichage ="points :"+str(nombre)
+	$VBoxContainer/HBoxContainer/colonneMain/pointsLettres.set_text(affichage)
